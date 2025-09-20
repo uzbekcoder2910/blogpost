8cc71a409e9508ff046da3470c73a901dad72d73
Adizov Numon <125878464+nomonadizov@users.noreply.github.com>
2025-09-20 18:38:22 +0500

    second commit

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/',include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
 ]
